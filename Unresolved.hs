8a8d92a7f861c08347074d4c3e109bf439716b8b
Kevin P. Barry <ta0kira@gmail.com>
2018-10-03 22:55:13 -0400

    Adds forgotten file.

---
+++ Unresolved.hs
@@ -0,0 +1,45 @@
+module Unresolved (
+  UnresolvedParamFilter(..),
+  UnresolvedType(..),
+  UnresolvedTypeClass(..),
+  UnresolvedTypeParam(..),
+) where
+
+import Control.Monad.Fix (fix)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+import Variance
+
+data UnresolvedTypeClass =
+  UnresolvedTypeClass {
+    utcName :: String,
+    utcParams :: [UnresolvedTypeParam],
+    utcInherits :: [UnresolvedType],
+    utcFilters :: [UnresolvedParamFilter]
+  }
+  deriving (Eq, Read, Show)
+
+data UnresolvedType =
+  UnresolvedType {
+    utTypeClass :: String,
+    utParamArgs :: [UnresolvedType]
+  } |
+  UnresolvedTypeArg {
+    utName :: String
+  }
+  deriving (Eq, Read, Show)
+
+data UnresolvedParamFilter =
+  UnresolvedParamFilter {
+    upfName :: String,
+    upfType :: UnresolvedType
+  }
+  deriving (Eq, Read, Show)
+
+data UnresolvedTypeParam =
+  UnresolvedTypeParam {
+    utpName :: String,
+    utpVariance :: Variance
+  }
+  deriving (Eq, Read, Show)
