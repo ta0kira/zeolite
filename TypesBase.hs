e15a57180b104b2cd9cc65846b14db536f21c7e5
Kevin P. Barry <ta0kira@gmail.com>
2018-11-09 22:53:21 -0500

    Fixes qualifications for union/intersection result merging.

--- TypesBase.hs
+++ TypesBase.hs
@@ -39,10 +39,10 @@ data ParamSet a =
 checkTypeInstance :: Mergeable c => (a -> b -> c) -> TypeInstance a -> TypeInstance b -> c
 checkTypeInstance f ti1 ti2 = singleCheck ti1 ti2 where
   singleCheck (TypeInstance t1) (TypeInstance t2) = t1 `f` t2
-  singleCheck (TypeMerge MergeUnion     t1) ti2 = mergeAny $ map (`singleCheck` ti2) t1
-  singleCheck (TypeMerge MergeIntersect t1) ti2 = mergeAll $ map (`singleCheck` ti2) t1
-  singleCheck ti1 (TypeMerge MergeUnion     t2) = mergeAll $ map (ti1 `singleCheck`) t2
-  singleCheck ti1 (TypeMerge MergeIntersect t2) = mergeAny $ map (ti1 `singleCheck`) t2
+  singleCheck (TypeMerge MergeUnion     t1) ti2 = mergeAll $ map (`singleCheck` ti2) t1
+  singleCheck (TypeMerge MergeIntersect t1) ti2 = mergeAny $ map (`singleCheck` ti2) t1
+  singleCheck ti1 (TypeMerge MergeUnion     t2) = mergeAny $ map (ti1 `singleCheck`) t2
+  singleCheck ti1 (TypeMerge MergeIntersect t2) = mergeAll $ map (ti1 `singleCheck`) t2
 
 checkParamsMatch :: (Mergeable c, CompileError c) => (a -> b -> c) -> ParamSet a -> ParamSet b -> c
 checkParamsMatch f (ParamSet ps1) (ParamSet ps2) = checkedMerge ps1 ps2 where
