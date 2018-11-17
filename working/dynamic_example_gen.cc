7fef667253ec518af58a6d66adb516c931f1684f
Kevin P. Barry <ta0kira@gmail.com>
2018-11-17 15:23:42 -0500

    Adds prototype of instantiation and conversion.

--- working/dynamic_example_gen.cc
+++ working/dynamic_example_gen.cc
@@ -1,9 +1,10 @@
 #include <functional>
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
 #include <tuple>
-#include <type_traits>
+#include <vector>
 
 #include "base.h"
 
@@ -13,18 +14,58 @@ using T = std::tuple<Ts...>;
 template<class...Ts>
 T<Ts...> T_get(Ts... ts) { return std::make_tuple(ts...); }
 
+struct TypeValue;
+struct TypeInstance;
+
+using InstanceCacheKey = std::vector<const TypeInstance*>;
+using InstanceCache = std::map<InstanceCacheKey,S<const TypeInstance>>;
+
+// TODO: Probably needs to use shared_ptr elements so that the types can be
+// referenced in conversion wrappers.
+using TypeArgs = std::vector<const TypeInstance*>;
+
+using FunctionId = std::string;
+using FunctionArgs = std::vector<S<TypeValue>>;
+using FunctionReturns = std::vector<S<TypeValue>>;
+
+struct TypeId {};
 
 struct TypeInstance {
   virtual std::string TypeName() const = 0;
+  virtual const TypeId* BaseType() const = 0;
+  virtual TypeArgs ConstructorArgs() const = 0;
+
+  virtual FunctionReturns CallStaticFunction(const FunctionId& id, const FunctionArgs&) const {
+    FAIL() << "Static function " << id << " not supported in " << TypeName();
+    return FunctionReturns();
+  }
+
   virtual ~TypeInstance() = default;
+};
+
+struct TypeValue {
+  virtual const TypeInstance* ValueType() const = 0;
+
+  virtual S<TypeValue> ConvertTo(const S<const TypeInstance>& type) {
+    FAIL() << "Cannot convert " << ValueType()->TypeName()
+           << " to " << type->TypeName();
+    return nullptr;
+  }
+
+  virtual FunctionReturns CallInstanceFunction(const FunctionId& id, const FunctionArgs&) {
+    FAIL() << "Instance function " << id << " not supported in " << ValueType()->TypeName();
+    return FunctionReturns();
+  }
+
+  virtual ~TypeValue() = default;
 };
 
 template<class...Ts>
 struct TypeConstructor {
-  virtual S<const TypeInstance> Build(const T<Ts...>&) const = 0;
-
-  S<const TypeInstance> AutoBuild(Ts... ts) const {
-    return Build(T_get(ts...));
+  virtual S<const TypeInstance> BindAll(const T<Ts...>&) = 0;
+  virtual const TypeId* BaseType() const = 0;
+  virtual S<const TypeInstance> Build(Ts... ts) {
+    return BindAll(T_get(ts...));
   }
 
   virtual ~TypeConstructor() = default;
@@ -46,8 +87,13 @@ struct ParamInstance<0, Ts...> {
 
 template<int N, int K>
 struct Select : public ParamInstance<N>::Type {
-  S<const TypeInstance> Build(const typename ParamInstance<N>::Args& args) const final {
+  S<const TypeInstance> BindAll(const typename ParamInstance<N>::Args& args) final {
     return std::get<K>(args);
+  }
+
+  const TypeId* BaseType() const final {
+    static class : public TypeId {} type;
+    return &type;
   }
 
   static S<typename ParamInstance<N>::Type> New() {
@@ -62,12 +108,16 @@ class Composer : public ParamInstance<N>::Type {
       : outer_(outer),
       inner_([inner...](const typename ParamInstance<N>::Args& args) ->
                  typename ParamInstance<M>::Args {
-               return T_get(inner->Build(args)...);
+               return T_get(inner->BindAll(args)...);
              })
       {}
 
-  S<const TypeInstance> Build(const typename ParamInstance<N>::Args& args) const final {
-    return outer_->Build(inner_(args));
+  const TypeId* BaseType() const final {
+    return outer_->BaseType();
+  }
+
+  S<const TypeInstance> BindAll(const typename ParamInstance<N>::Args& args) final {
+    return outer_->BindAll(inner_(args));
   }
 
  private:
@@ -90,10 +140,25 @@ interface Function<x|y> {
 
 extern const S<ParamInstance<2>::Type> Function;
 
+class : public TypeId {} function_type;
+
 class FunctionT : public ParamInstance<2>::Type {
  public:
-  S<const TypeInstance> Build(const ParamInstance<2>::Args& args) const final {
-    return S_get(new Instance(std::get<0>(args), std::get<1>(args)));
+  S<const TypeInstance> BindAll(const ParamInstance<2>::Args& args) final {
+    S<const TypeInstance>& instance =
+        instance_cache_[InstanceCacheKey{std::get<0>(args).get(),std::get<1>(args).get()}];
+    if (!instance) {
+      instance = S_get(new Instance(std::get<0>(args), std::get<1>(args)));
+      std::cerr << "New: " << instance->TypeName() << std::endl;
+      return instance;
+    } else {
+      std::cerr << "From cache: " << instance->TypeName() << std::endl;
+    }
+    return instance;
+  }
+
+  const TypeId* BaseType() const final {
+    return &function_type;
   }
 
  private:
@@ -107,10 +172,20 @@ class FunctionT : public ParamInstance<2>::Type {
       return formatted.str();
     }
 
+    const TypeId* BaseType() const final {
+      return &function_type;
+    }
+
+    TypeArgs ConstructorArgs() const final {
+      return TypeArgs{x_.get(),y_.get()};
+    }
+
    private:
     const S<const TypeInstance> x_;
     const S<const TypeInstance> y_;
   };
+
+  InstanceCache instance_cache_;
 };
 
 const S<ParamInstance<2>::Type> Function(new FunctionT);
@@ -124,10 +199,25 @@ interface Data<x> {
 
 extern const S<ParamInstance<1>::Type> Data;
 
+class : public TypeId {} data_type;
+
 class DataT : public ParamInstance<1>::Type {
  public:
-  S<const TypeInstance> Build(const ParamInstance<1>::Args& args) const final {
-    return S_get(new Instance(std::get<0>(args)));
+  S<const TypeInstance> BindAll(const ParamInstance<1>::Args& args) final {
+    S<const TypeInstance>& instance =
+        instance_cache_[InstanceCacheKey{std::get<0>(args).get()}];
+    if (!instance) {
+      instance = S_get(new Instance(std::get<0>(args)));
+      std::cerr << "New: " << instance->TypeName() << std::endl;
+      return instance;
+    } else {
+      std::cerr << "From cache: " << instance->TypeName() << std::endl;
+    }
+    return instance;
+  }
+
+  const TypeId* BaseType() const final {
+    return &data_type;
   }
 
  private:
@@ -141,9 +231,19 @@ class DataT : public ParamInstance<1>::Type {
       return formatted.str();
     }
 
+    const TypeId* BaseType() const final {
+      return &data_type;
+    }
+
+    TypeArgs ConstructorArgs() const final {
+      return TypeArgs{x_.get()};
+    }
+
    private:
     const S<const TypeInstance> x_;
   };
+
+  InstanceCache instance_cache_;
 };
 
 const S<ParamInstance<1>::Type> Data(new DataT);
@@ -151,16 +251,23 @@ const S<ParamInstance<1>::Type> Data(new DataT);
 /*
 
 interface Value {
+  inherits Data<Value>
 }
 
 */
 
 extern const S<ParamInstance<0>::Type> Value;
 
+class : public TypeId {} value_type;
+
 class ValueT : public ParamInstance<0>::Type {
  public:
-  S<const TypeInstance> Build(const ParamInstance<0>::Args& args) const final {
-    return S_get(new Instance());
+  S<const TypeInstance> BindAll(const ParamInstance<0>::Args& args) final {
+    return only_instance_;
+  }
+
+  const TypeId* BaseType() const final {
+    return &value_type;
   }
 
  private:
@@ -169,7 +276,59 @@ class ValueT : public ParamInstance<0>::Type {
     std::string TypeName() const final {
       return "Value";
     }
+
+    const TypeId* BaseType() const final {
+      return &value_type;
+    }
+
+    FunctionReturns CallStaticFunction(const FunctionId& id, const FunctionArgs& args) const final {
+      if (id == "create") {
+        FAIL_IF(args.size() != 0) << "Too many args passed to 'Value.create'";
+        return FunctionReturns{S_get(new Value(*this))};
+      }
+      return TypeInstance::CallStaticFunction(id, args);
+    }
+
+    TypeArgs ConstructorArgs() const final {
+      return TypeArgs{};
+    }
   };
+
+  class Value : public TypeValue {
+   public:
+    Value(const Instance& type) : type_(type) {}
+
+    const TypeInstance* ValueType() const final {
+      return &type_;
+    }
+
+    S<TypeValue> ConvertTo(const S<const TypeInstance>& type) override {
+      if (type->BaseType() == Data->BaseType() && type->ConstructorArgs() == TypeArgs{&type_}) {
+        // TODO: Handle conversion.
+        return S_get(new Value_Data(*type));
+      }
+      return TypeValue::ConvertTo(type);
+    }
+
+   private:
+    const Instance& type_;
+  };
+
+  // TODO: Does Data need to provide this wrapper (or an abstract interface)
+  // for its own conversions?
+  class Value_Data : public TypeValue {
+   public:
+    Value_Data(const TypeInstance& type) : type_(type) {}
+
+    const TypeInstance* ValueType() const final {
+      return &type_;
+    }
+
+   private:
+    const TypeInstance& type_;
+  };
+
+  const S<const TypeInstance> only_instance_ = S_get(new Instance());
 };
 
 const S<ParamInstance<0>::Type> Value(new ValueT);
@@ -189,13 +348,18 @@ Function<Data<Value>,Value>
 */
 
 // Using composition, e.g., within a function that creates Function<Data<x>,x>.
-const S<const TypeInstance> ValueDataFunction = DataFunction->AutoBuild(Value->AutoBuild());
+const S<const TypeInstance> ValueDataFunction = DataFunction->Build(Value->Build());
 
 // Without composition, i.e., literally Function<Data<Value>,Value>.
-const S<const TypeInstance> ValueDataFunction2 = Function->AutoBuild(Data->AutoBuild(Value->AutoBuild()), Value->AutoBuild());
+const S<const TypeInstance> ValueDataFunction2 = Function->Build(Data->Build(Value->Build()), Value->Build());
 
 
 int main() {
   std::cerr << ValueDataFunction->TypeName() << std::endl;
   std::cerr << ValueDataFunction2->TypeName() << std::endl;
+  const S<const TypeInstance> v_type = Value->Build();
+  S<TypeValue> v = v_type->CallStaticFunction("create", FunctionArgs{})[0];
+  std::cerr << v->ValueType()->TypeName() << std::endl;
+  auto v2 = v->ConvertTo(Data->Build(Value->Build()));
+  std::cerr << v2->ValueType()->TypeName() << std::endl;
 }
