54f3d04779d9ee2b1e4ed7c3fa80209f51f4c4bd
Kevin P. Barry <ta0kira@gmail.com>
2020-01-14 12:04:22 +0100

    Renames CompileError constructor to CompileFail since the former is also a class name.

--- compiler/CompileInfo.hs
+++ compiler/CompileInfo.hs
@@ -46,60 +46,60 @@ instance Show CompileMessage where
     doIndent indent s = intercalate "\n" $ map (indent ++) $ lines s
 
 data CompileInfo a =
-  CompileError {
+  CompileFail {
     ceErrors :: CompileMessage
   } |
   CompileSuccess {
     csData :: a
   }
 
-isCompileError (CompileError _) = True
+isCompileError (CompileFail _) = True
 isCompileError _                = False
 
 getCompileError   = ceErrors
 getCompileSuccess = csData
 
 instance Functor CompileInfo where
-  fmap f (CompileError e)   = (CompileError e) -- Not the same a.
+  fmap f (CompileFail e)    = (CompileFail e) -- Not the same a.
   fmap f (CompileSuccess d) = CompileSuccess (f d)
 
 instance Applicative CompileInfo where
   pure = CompileSuccess
-  (CompileError e) <*> _ = (CompileError e) -- Not the same a.
-  _ <*> (CompileError e) = (CompileError e) -- Not the same a.
+  (CompileFail e) <*> _ = (CompileFail e) -- Not the same a.
+  _ <*> (CompileFail e) = (CompileFail e) -- Not the same a.
   (CompileSuccess f) <*> (CompileSuccess d) = CompileSuccess (f d)
 
 instance Monad CompileInfo where
-  (CompileError e)   >>= _ = (CompileError e) -- Not the same a.
+  (CompileFail e)    >>= _ = (CompileFail e) -- Not the same a.
   (CompileSuccess d) >>= f = f d
   return = CompileSuccess
 
 instance CompileErrorM CompileInfo where
-  compileErrorM = CompileError . flip CompileMessage []
+  compileErrorM = CompileFail . flip CompileMessage []
   isCompileErrorM = isCompileError
   collectAllOrErrorM = result . splitErrorsAndData where
     result ([],xs) = return xs
-    result (es,_) = CompileError $ CompileMessage "" es
+    result (es,_) = CompileFail $ CompileMessage "" es
   collectOneOrErrorM = result . splitErrorsAndData where
     result (_,x:_) = return x
     result ([],_)  = compileErrorM "No choices found"
-    result (es,_)  = CompileError $ CompileMessage "" es
+    result (es,_)  = CompileFail $ CompileMessage "" es
   reviseErrorM x@(CompileSuccess _) _ = x
-  reviseErrorM x@(CompileError (CompileMessage [] ms)) s = CompileError $ CompileMessage s ms
-  reviseErrorM x@(CompileError e) s = CompileError $ CompileMessage s [e]
+  reviseErrorM x@(CompileFail (CompileMessage [] ms)) s = CompileFail $ CompileMessage s ms
+  reviseErrorM x@(CompileFail e) s = CompileFail $ CompileMessage s [e]
 
 instance MergeableM CompileInfo where
   mergeAnyM = result . splitErrorsAndData where
     result (_,xs@(x:_)) = return $ mergeAny xs
     result ([],_)       = compileErrorM "No choices found"
-    result (es,_)       = CompileError $ CompileMessage "" es
+    result (es,_)       = CompileFail $ CompileMessage "" es
   mergeAllM = result . splitErrorsAndData where
     result ([],xs) = return $ mergeAll xs
-    result (es,_)  = CompileError $ CompileMessage "" es
+    result (es,_)  = CompileFail $ CompileMessage "" es
   (CompileSuccess x) `mergeNestedM` (CompileSuccess y) = return $ x `mergeNested` y
-  e@(CompileError _) `mergeNestedM` (CompileSuccess _) = e
-  (CompileSuccess _) `mergeNestedM` e@(CompileError _) = e
-  (CompileError e1)  `mergeNestedM` (CompileError e2)  = CompileError $ e1 `nestMessages` e2
+  e@(CompileFail _)  `mergeNestedM` (CompileSuccess _) = e
+  (CompileSuccess _) `mergeNestedM` e@(CompileFail _)  = e
+  (CompileFail e1)   `mergeNestedM` (CompileFail e2)   = CompileFail $ e1 `nestMessages` e2
 
 nestMessages (CompileMessage m1 ms1) (CompileMessage [] ms2) =
   CompileMessage m1 (ms1 ++ ms2)
@@ -110,5 +110,5 @@ nestMessages (CompileMessage m1 ms1) ma@(CompileMessage _ _) =
 
 splitErrorsAndData :: Foldable f => f (CompileInfo a) -> ([CompileMessage],[a])
 splitErrorsAndData = foldr partition ([],[]) where
-  partition (CompileError e)   (es,ds) = (e:es,ds)
+  partition (CompileFail e)    (es,ds) = (e:es,ds)
   partition (CompileSuccess d) (es,ds) = (es,d:ds)
