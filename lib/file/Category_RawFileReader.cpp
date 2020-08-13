6a1df8f67cb3a3f9a0d76703d554a6ec52decd85
Kevin P. Barry <ta0kira@gmail.com>
2020-08-13 14:19:24 -0700

    Updates lib/file with shared TypeInstance. [Issue #48]

--- lib/file/Category_RawFileReader.cpp
+++ lib/file/Category_RawFileReader.cpp
@@ -1,12 +1,9 @@
 /* -----------------------------------------------------------------------------
 Copyright 2020 Kevin P. Barry
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,30 +16,28 @@ limitations under the License.
 #include <fstream>
 
 #include "category-source.hpp"
-#include "Category_String.hpp"
 #include "Category_BlockReader.hpp"
+#include "Category_Bool.hpp"
+#include "Category_Formatted.hpp"
+#include "Category_Int.hpp"
 #include "Category_PersistentResource.hpp"
 #include "Category_RawFileReader.hpp"
-
-
+#include "Category_String.hpp"
 #ifdef ZEOLITE_PUBLIC_NAMESPACE
 namespace ZEOLITE_PUBLIC_NAMESPACE {
 #endif  // ZEOLITE_PUBLIC_NAMESPACE
-
 namespace {
-const int collection = 0;
+const int collection_RawFileReader = 0;
 }  // namespace
-
-const void* const Functions_RawFileReader = &collection;
+const void* const Functions_RawFileReader = &collection_RawFileReader;
 const TypeFunction& Function_RawFileReader_open = (*new TypeFunction{ 0, 1, 1, "RawFileReader", "open", Functions_RawFileReader, 0 });
 const ValueFunction& Function_RawFileReader_getFileError = (*new ValueFunction{ 0, 0, 1, "RawFileReader", "getFileError", Functions_RawFileReader, 0 });
-
 namespace {
 class Category_RawFileReader;
 class Type_RawFileReader;
-Type_RawFileReader& CreateType(Params<0>::Type params);
+S<Type_RawFileReader> CreateType_RawFileReader(Params<0>::Type params);
 class Value_RawFileReader;
-S<TypeValue> CreateValue(Type_RawFileReader& parent, const ParamTuple& params, const ValueTuple& args);
+S<TypeValue> CreateValue_RawFileReader(S<Type_RawFileReader> parent, const ParamTuple& params, const ValueTuple& args);
 struct Category_RawFileReader : public TypeCategory {
   std::string CategoryName() const final { return "RawFileReader"; }
   Category_RawFileReader() {
@@ -55,7 +50,7 @@ struct Category_RawFileReader : public TypeCategory {
     return TypeCategory::Dispatch(label, params, args);
   }
 };
-Category_RawFileReader& CreateCategory() {
+Category_RawFileReader& CreateCategory_RawFileReader() {
   static auto& category = *new Category_RawFileReader();
   return category;
 }
@@ -66,24 +61,24 @@ struct Type_RawFileReader : public TypeInstance {
   }
   Category_RawFileReader& parent;
   bool CanConvertFrom(const TypeInstance& from) const final {
-    std::vector<const TypeInstance*> args;
+    std::vector<S<const TypeInstance>> args;
     if (!from.TypeArgsForParent(parent, args)) return false;
     if(args.size() != 0) {
       FAIL() << "Wrong number of args (" << args.size() << ")  for " << CategoryName();
     }
     return true;
   }
-  bool TypeArgsForParent(const TypeCategory& category, std::vector<const TypeInstance*>& args) const final {
+  bool TypeArgsForParent(const TypeCategory& category, std::vector<S<const TypeInstance>>& args) const final {
     if (&category == &GetCategory_RawFileReader()) {
-      args = std::vector<const TypeInstance*>{};
+      args = std::vector<S<const TypeInstance>>{};
       return true;
     }
     if (&category == &GetCategory_PersistentResource()) {
-      args = std::vector<const TypeInstance*>{};
+      args = std::vector<S<const TypeInstance>>{};
       return true;
     }
     if (&category == &GetCategory_BlockReader()) {
-      args = std::vector<const TypeInstance*>{&GetType_String(T_get())};
+      args = std::vector<S<const TypeInstance>>{GetType_String(T_get())};
       return true;
     }
     return false;
@@ -93,8 +88,8 @@ struct Type_RawFileReader : public TypeInstance {
     CycleCheck<Type_RawFileReader> marker(*this);
     TRACE_FUNCTION("RawFileReader (init @type)")
   }
-  ReturnTuple Dispatch(const TypeFunction& label, const ParamTuple& params, const ValueTuple& args) final {
-    using CallType = ReturnTuple(Type_RawFileReader::*)(const ParamTuple&, const ValueTuple&);
+  ReturnTuple Dispatch(const S<TypeInstance>& self, const TypeFunction& label, const ParamTuple& params, const ValueTuple& args) final {
+    using CallType = ReturnTuple(Type_RawFileReader::*)(const S<TypeInstance>&, const ParamTuple&, const ValueTuple&);
     static const CallType Table_RawFileReader[] = {
       &Type_RawFileReader::Call_open,
     };
@@ -102,21 +97,20 @@ struct Type_RawFileReader : public TypeInstance {
       if (label.function_num < 0 || label.function_num >= 1) {
         FAIL() << "Bad function call " << label;
       }
-      return (this->*Table_RawFileReader[label.function_num])(params, args);
+      return (this->*Table_RawFileReader[label.function_num])(self, params, args);
     }
-    return TypeInstance::Dispatch(label, params, args);
+    return TypeInstance::Dispatch(self, label, params, args);
   }
-  ReturnTuple Call_open(const ParamTuple& params, const ValueTuple& args);
+  ReturnTuple Call_open(const S<TypeInstance>& self, const ParamTuple& params, const ValueTuple& args);
 };
-Type_RawFileReader& CreateType(Params<0>::Type params) {
-  static auto& cached = *new Type_RawFileReader(CreateCategory(), Params<0>::Type());
+S<Type_RawFileReader> CreateType_RawFileReader(Params<0>::Type params) {
+  static const auto cached = S_get(new Type_RawFileReader(CreateCategory_RawFileReader(), Params<0>::Type()));
   return cached;
 }
 struct Value_RawFileReader : public TypeValue {
-  Value_RawFileReader(Type_RawFileReader& p, const ParamTuple& params, const ValueTuple& args)
+  Value_RawFileReader(S<Type_RawFileReader> p, const ParamTuple& params, const ValueTuple& args)
     : parent(p), filename(args.At(0)->AsString()),
-    file(new std::ifstream(filename, std::ios::in | std::ios::binary)) {}
-
+      file(new std::ifstream(filename, std::ios::in | std::ios::binary)) {}
   ReturnTuple Dispatch(const S<TypeValue>& self, const ValueFunction& label, const ParamTuple& params,const ValueTuple& args) final {
     using CallType = ReturnTuple(Value_RawFileReader::*)(const S<TypeValue>&, const ParamTuple&, const ValueTuple&);
     static const CallType Table_BlockReader[] = {
@@ -149,27 +143,24 @@ struct Value_RawFileReader : public TypeValue {
     }
     return TypeValue::Dispatch(self, label, params, args);
   }
-  std::string CategoryName() const final { return parent.CategoryName(); }
+  std::string CategoryName() const final { return parent->CategoryName(); }
   ReturnTuple Call_freeResource(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args);
   ReturnTuple Call_getFileError(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args);
   ReturnTuple Call_pastEnd(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args);
   ReturnTuple Call_readBlock(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args);
-  Type_RawFileReader& parent;
-
+  const S<Type_RawFileReader> parent;
   std::mutex mutex;
   const std::string filename;
   R<std::istream> file;
   CAPTURE_CREATION
 };
-S<TypeValue> CreateValue(Type_RawFileReader& parent, const ParamTuple& params, const ValueTuple& args) {
+S<TypeValue> CreateValue_RawFileReader(S<Type_RawFileReader> parent, const ParamTuple& params, const ValueTuple& args) {
   return S_get(new Value_RawFileReader(parent, params, args));
 }
-
-ReturnTuple Type_RawFileReader::Call_open(const ParamTuple& params, const ValueTuple& args) {
-  TRACE_FUNCTION("RawFileReader.create")
-  return ReturnTuple(CreateValue(*this, ParamTuple(), args));
+ReturnTuple Type_RawFileReader::Call_open(const S<TypeInstance>& self, const ParamTuple& params, const ValueTuple& args) {
+  TRACE_FUNCTION("RawFileReader.open")
+  return ReturnTuple(CreateValue_RawFileReader(CreateType_RawFileReader(Params<0>::Type()), ParamTuple(), args));
 }
-
 ReturnTuple Value_RawFileReader::Call_freeResource(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args) {
   TRACE_FUNCTION("RawFileReader.freeResource")
   std::lock_guard<std::mutex> lock(mutex);
@@ -178,7 +169,6 @@ ReturnTuple Value_RawFileReader::Call_freeResource(const S<TypeValue>& Var_self,
   }
   return ReturnTuple();
 }
-
 ReturnTuple Value_RawFileReader::Call_getFileError(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args) {
   TRACE_FUNCTION("RawFileReader.getFileError")
   std::lock_guard<std::mutex> lock(mutex);
@@ -193,13 +183,11 @@ ReturnTuple Value_RawFileReader::Call_getFileError(const S<TypeValue>& Var_self,
   }
   return ReturnTuple(Var_empty);
 }
-
 ReturnTuple Value_RawFileReader::Call_pastEnd(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args) {
   TRACE_FUNCTION("RawFileReader.pastEnd")
   std::lock_guard<std::mutex> lock(mutex);
   return ReturnTuple(Box_Bool(!file || file->fail() || file->eof()));
 }
-
 ReturnTuple Value_RawFileReader::Call_readBlock(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args) {
   TRACE_FUNCTION("RawFileReader.readBlock")
   TRACE_CREATION
@@ -228,15 +216,13 @@ ReturnTuple Value_RawFileReader::Call_readBlock(const S<TypeValue>& Var_self, co
   }
   return ReturnTuple(Box_String(buffer.substr(0, read_size)));
 }
-
 }  // namespace
 TypeCategory& GetCategory_RawFileReader() {
-  return CreateCategory();
+  return CreateCategory_RawFileReader();
 }
-TypeInstance& GetType_RawFileReader(Params<0>::Type params) {
-  return CreateType(params);
+S<TypeInstance> GetType_RawFileReader(Params<0>::Type params) {
+  return CreateType_RawFileReader(params);
 }
-
 #ifdef ZEOLITE_PUBLIC_NAMESPACE
 }  // namespace ZEOLITE_PUBLIC_NAMESPACE
 using namespace ZEOLITE_PUBLIC_NAMESPACE;
--- lib/file/Category_RawFileWriter.cpp
+++ lib/file/Category_RawFileWriter.cpp
@@ -1,12 +1,10 @@
+
 /* -----------------------------------------------------------------------------
 Copyright 2020 Kevin P. Barry
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,30 +17,27 @@ limitations under the License.
 #include <fstream>
 
 #include "category-source.hpp"
-#include "Category_String.hpp"
 #include "Category_BlockWriter.hpp"
+#include "Category_Formatted.hpp"
+#include "Category_Int.hpp"
 #include "Category_PersistentResource.hpp"
 #include "Category_RawFileWriter.hpp"
-
-
+#include "Category_String.hpp"
 #ifdef ZEOLITE_PUBLIC_NAMESPACE
 namespace ZEOLITE_PUBLIC_NAMESPACE {
 #endif  // ZEOLITE_PUBLIC_NAMESPACE
-
 namespace {
-const int collection = 0;
+const int collection_RawFileWriter = 0;
 }  // namespace
-
-const void* const Functions_RawFileWriter = &collection;
+const void* const Functions_RawFileWriter = &collection_RawFileWriter;
 const TypeFunction& Function_RawFileWriter_open = (*new TypeFunction{ 0, 1, 1, "RawFileWriter", "open", Functions_RawFileWriter, 0 });
 const ValueFunction& Function_RawFileWriter_getFileError = (*new ValueFunction{ 0, 0, 1, "RawFileWriter", "getFileError", Functions_RawFileWriter, 0 });
-
 namespace {
 class Category_RawFileWriter;
 class Type_RawFileWriter;
-Type_RawFileWriter& CreateType(Params<0>::Type params);
+S<Type_RawFileWriter> CreateType_RawFileWriter(Params<0>::Type params);
 class Value_RawFileWriter;
-S<TypeValue> CreateValue(Type_RawFileWriter& parent, const ParamTuple& params, const ValueTuple& args);
+S<TypeValue> CreateValue_RawFileWriter(S<Type_RawFileWriter> parent, const ParamTuple& params, const ValueTuple& args);
 struct Category_RawFileWriter : public TypeCategory {
   std::string CategoryName() const final { return "RawFileWriter"; }
   Category_RawFileWriter() {
@@ -55,7 +50,7 @@ struct Category_RawFileWriter : public TypeCategory {
     return TypeCategory::Dispatch(label, params, args);
   }
 };
-Category_RawFileWriter& CreateCategory() {
+Category_RawFileWriter& CreateCategory_RawFileWriter() {
   static auto& category = *new Category_RawFileWriter();
   return category;
 }
@@ -66,24 +61,24 @@ struct Type_RawFileWriter : public TypeInstance {
   }
   Category_RawFileWriter& parent;
   bool CanConvertFrom(const TypeInstance& from) const final {
-    std::vector<const TypeInstance*> args;
+    std::vector<S<const TypeInstance>> args;
     if (!from.TypeArgsForParent(parent, args)) return false;
     if(args.size() != 0) {
       FAIL() << "Wrong number of args (" << args.size() << ")  for " << CategoryName();
     }
     return true;
   }
-  bool TypeArgsForParent(const TypeCategory& category, std::vector<const TypeInstance*>& args) const final {
+  bool TypeArgsForParent(const TypeCategory& category, std::vector<S<const TypeInstance>>& args) const final {
     if (&category == &GetCategory_RawFileWriter()) {
-      args = std::vector<const TypeInstance*>{};
+      args = std::vector<S<const TypeInstance>>{};
       return true;
     }
     if (&category == &GetCategory_PersistentResource()) {
-      args = std::vector<const TypeInstance*>{};
+      args = std::vector<S<const TypeInstance>>{};
       return true;
     }
     if (&category == &GetCategory_BlockWriter()) {
-      args = std::vector<const TypeInstance*>{&GetType_String(T_get())};
+      args = std::vector<S<const TypeInstance>>{GetType_String(T_get())};
       return true;
     }
     return false;
@@ -93,8 +88,8 @@ struct Type_RawFileWriter : public TypeInstance {
     CycleCheck<Type_RawFileWriter> marker(*this);
     TRACE_FUNCTION("RawFileWriter (init @type)")
   }
-  ReturnTuple Dispatch(const TypeFunction& label, const ParamTuple& params, const ValueTuple& args) final {
-    using CallType = ReturnTuple(Type_RawFileWriter::*)(const ParamTuple&, const ValueTuple&);
+  ReturnTuple Dispatch(const S<TypeInstance>& self, const TypeFunction& label, const ParamTuple& params, const ValueTuple& args) final {
+    using CallType = ReturnTuple(Type_RawFileWriter::*)(const S<TypeInstance>&, const ParamTuple&, const ValueTuple&);
     static const CallType Table_RawFileWriter[] = {
       &Type_RawFileWriter::Call_open,
     };
@@ -102,21 +97,20 @@ struct Type_RawFileWriter : public TypeInstance {
       if (label.function_num < 0 || label.function_num >= 1) {
         FAIL() << "Bad function call " << label;
       }
-      return (this->*Table_RawFileWriter[label.function_num])(params, args);
+      return (this->*Table_RawFileWriter[label.function_num])(self, params, args);
     }
-    return TypeInstance::Dispatch(label, params, args);
+    return TypeInstance::Dispatch(self, label, params, args);
   }
-  ReturnTuple Call_open(const ParamTuple& params, const ValueTuple& args);
+  ReturnTuple Call_open(const S<TypeInstance>& self, const ParamTuple& params, const ValueTuple& args);
 };
-Type_RawFileWriter& CreateType(Params<0>::Type params) {
-  static auto& cached = *new Type_RawFileWriter(CreateCategory(), Params<0>::Type());
+S<Type_RawFileWriter> CreateType_RawFileWriter(Params<0>::Type params) {
+  static const auto cached = S_get(new Type_RawFileWriter(CreateCategory_RawFileWriter(), Params<0>::Type()));
   return cached;
 }
 struct Value_RawFileWriter : public TypeValue {
-  Value_RawFileWriter(Type_RawFileWriter& p, const ParamTuple& params, const ValueTuple& args)
+  Value_RawFileWriter(S<Type_RawFileWriter> p, const ParamTuple& params, const ValueTuple& args)
     : parent(p), filename(args.At(0)->AsString()),
-    file(new std::ofstream(filename, std::ios::out | std::ios::binary | std::ios::trunc | std::ios::ate)) {}
-
+      file(new std::ofstream(filename, std::ios::out | std::ios::binary | std::ios::trunc | std::ios::ate)) {}
   ReturnTuple Dispatch(const S<TypeValue>& self, const ValueFunction& label, const ParamTuple& params,const ValueTuple& args) final {
     using CallType = ReturnTuple(Value_RawFileWriter::*)(const S<TypeValue>&, const ParamTuple&, const ValueTuple&);
     static const CallType Table_BlockWriter[] = {
@@ -148,24 +142,22 @@ struct Value_RawFileWriter : public TypeValue {
     }
     return TypeValue::Dispatch(self, label, params, args);
   }
-  std::string CategoryName() const final { return parent.CategoryName(); }
+  std::string CategoryName() const final { return parent->CategoryName(); }
   ReturnTuple Call_freeResource(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args);
   ReturnTuple Call_getFileError(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args);
   ReturnTuple Call_writeBlock(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args);
-  Type_RawFileWriter& parent;
-
+  const S<Type_RawFileWriter> parent;
   std::mutex mutex;
   const std::string filename;
   R<std::ostream> file;
   CAPTURE_CREATION
 };
-S<TypeValue> CreateValue(Type_RawFileWriter& parent, const ParamTuple& params, const ValueTuple& args) {
+S<TypeValue> CreateValue_RawFileWriter(S<Type_RawFileWriter> parent, const ParamTuple& params, const ValueTuple& args) {
   return S_get(new Value_RawFileWriter(parent, params, args));
 }
-
-ReturnTuple Type_RawFileWriter::Call_open(const ParamTuple& params, const ValueTuple& args) {
-  TRACE_FUNCTION("RawFileWriter.create")
-  return ReturnTuple(CreateValue(*this, ParamTuple(), args));
+ReturnTuple Type_RawFileWriter::Call_open(const S<TypeInstance>& self, const ParamTuple& params, const ValueTuple& args) {
+  TRACE_FUNCTION("RawFileWriter.open")
+  return ReturnTuple(CreateValue_RawFileWriter(CreateType_RawFileWriter(Params<0>::Type()), ParamTuple(), args));
 }
 ReturnTuple Value_RawFileWriter::Call_freeResource(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args) {
   TRACE_FUNCTION("RawFileWriter.freeResource")
@@ -205,16 +197,13 @@ ReturnTuple Value_RawFileWriter::Call_writeBlock(const S<TypeValue>& Var_self, c
   }
   return ReturnTuple(Box_Int(write_size));
 }
-
 }  // namespace
-
 TypeCategory& GetCategory_RawFileWriter() {
-  return CreateCategory();
+  return CreateCategory_RawFileWriter();
 }
-TypeInstance& GetType_RawFileWriter(Params<0>::Type params) {
-  return CreateType(params);
+S<TypeInstance> GetType_RawFileWriter(Params<0>::Type params) {
+  return CreateType_RawFileWriter(params);
 }
-
 #ifdef ZEOLITE_PUBLIC_NAMESPACE
 }  // namespace ZEOLITE_PUBLIC_NAMESPACE
 using namespace ZEOLITE_PUBLIC_NAMESPACE;
