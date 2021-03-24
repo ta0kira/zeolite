05d3c7b2f05e775b9f759db62a9162e256fbb2c2
Kevin P. Barry <ta0kira@gmail.com>
2021-03-24 13:41:37 -0600

    Adds mutex support to lib/thread. [Issue #76]

---
+++ lib/thread/src/Extension_MutexLock.cpp
@@ -0,0 +1,91 @@
+/* -----------------------------------------------------------------------------
+Copyright 2021 Kevin P. Barry
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+----------------------------------------------------------------------------- */
+
+// Author: Kevin P. Barry [ta0kira@gmail.com]
+
+#include <mutex>
+
+#include "category-source.hpp"
+#include "Streamlined_MutexLock.hpp"
+#include "Category_Mutex.hpp"
+#include "Category_MutexLock.hpp"
+#include "Category_PersistentResource.hpp"
+
+#ifdef ZEOLITE_PUBLIC_NAMESPACE
+namespace ZEOLITE_PUBLIC_NAMESPACE {
+#endif  // ZEOLITE_PUBLIC_NAMESPACE
+
+S<TypeValue> CreateValue_MutexLock(S<Type_MutexLock> parent, const ParamTuple& params, const ValueTuple& args);
+
+struct ExtCategory_MutexLock : public Category_MutexLock {
+};
+
+struct ExtType_MutexLock : public Type_MutexLock {
+  inline ExtType_MutexLock(Category_MutexLock& p, Params<0>::Type params) : Type_MutexLock(p, params) {}
+
+  ReturnTuple Call_lock(const S<TypeInstance>& Param_self, const ParamTuple& params, const ValueTuple& args) final {
+    TRACE_FUNCTION("MutexLock.lock")
+    return ReturnTuple(CreateValue_MutexLock(CreateType_MutexLock(Params<0>::Type()), ParamTuple(), args));
+  }
+};
+
+struct ExtValue_MutexLock : public Value_MutexLock {
+  inline ExtValue_MutexLock(S<Type_MutexLock> p, const ParamTuple& params, const ValueTuple& args)
+    : Value_MutexLock(p, params), mutex(args.Only()) {
+    TypeValue::Call(mutex, Function_Mutex_lock, ParamTuple(), ArgTuple());
+  }
+
+  ReturnTuple Call_freeResource(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args) final {
+    TRACE_FUNCTION("PersistentResource.freeResource")
+    TRACE_CREATION
+    if (!mutex) {
+      FAIL() << "MutexLock freed multiple times";
+    } else {
+      TypeValue::Call(mutex, Function_Mutex_unlock, ParamTuple(), ArgTuple());
+      mutex = nullptr;
+    }
+    return ReturnTuple();
+  }
+
+  ~ExtValue_MutexLock() {
+    if (mutex) {
+      TRACE_CREATION
+      TypeValue::Call(mutex, Function_Mutex_unlock, ParamTuple(), ArgTuple());
+      mutex = nullptr;
+      FAIL() << "MutexLock not freed with freeResource()";
+    }
+  }
+
+  S<TypeValue> mutex;
+  CAPTURE_CREATION
+};
+
+Category_MutexLock& CreateCategory_MutexLock() {
+  static auto& category = *new ExtCategory_MutexLock();
+  return category;
+}
+S<Type_MutexLock> CreateType_MutexLock(Params<0>::Type params) {
+  static const auto cached = S_get(new ExtType_MutexLock(CreateCategory_MutexLock(), Params<0>::Type()));
+  return cached;
+}
+S<TypeValue> CreateValue_MutexLock(S<Type_MutexLock> parent, const ParamTuple& params, const ValueTuple& args) {
+  return S_get(new ExtValue_MutexLock(parent, params, args));
+}
+
+#ifdef ZEOLITE_PUBLIC_NAMESPACE
+}  // namespace ZEOLITE_PUBLIC_NAMESPACE
+using namespace ZEOLITE_PUBLIC_NAMESPACE;
+#endif  // ZEOLITE_PUBLIC_NAMESPACE
---
+++ lib/thread/src/Extension_SimpleMutex.cpp
@@ -0,0 +1,77 @@
+/* -----------------------------------------------------------------------------
+Copyright 2021 Kevin P. Barry
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+----------------------------------------------------------------------------- */
+
+// Author: Kevin P. Barry [ta0kira@gmail.com]
+
+#include <mutex>
+
+#include "category-source.hpp"
+#include "Streamlined_SimpleMutex.hpp"
+#include "Category_Mutex.hpp"
+#include "Category_SimpleMutex.hpp"
+
+#ifdef ZEOLITE_PUBLIC_NAMESPACE
+namespace ZEOLITE_PUBLIC_NAMESPACE {
+#endif  // ZEOLITE_PUBLIC_NAMESPACE
+
+S<TypeValue> CreateValue_SimpleMutex(S<Type_SimpleMutex> parent);
+
+struct ExtCategory_SimpleMutex : public Category_SimpleMutex {
+};
+
+struct ExtType_SimpleMutex : public Type_SimpleMutex {
+  inline ExtType_SimpleMutex(Category_SimpleMutex& p, Params<0>::Type params) : Type_SimpleMutex(p, params) {}
+
+  ReturnTuple Call_new(const S<TypeInstance>& Param_self, const ParamTuple& params, const ValueTuple& args) final {
+    TRACE_FUNCTION("SimpleMutex.new")
+    return ReturnTuple(CreateValue_SimpleMutex(CreateType_SimpleMutex(Params<0>::Type())));
+  }
+};
+
+struct ExtValue_SimpleMutex : public Value_SimpleMutex {
+  inline ExtValue_SimpleMutex(S<Type_SimpleMutex> p, const ParamTuple& params) : Value_SimpleMutex(p, params) {}
+
+  ReturnTuple Call_lock(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args) final {
+    TRACE_FUNCTION("Mutex.lock")
+    mutex.lock();
+    return ReturnTuple(Var_self);
+  }
+
+  ReturnTuple Call_unlock(const S<TypeValue>& Var_self, const ParamTuple& params, const ValueTuple& args) final {
+    TRACE_FUNCTION("Mutex.unlock")
+    mutex.unlock();
+    return ReturnTuple(Var_self);
+  }
+
+  std::mutex mutex;
+};
+
+Category_SimpleMutex& CreateCategory_SimpleMutex() {
+  static auto& category = *new ExtCategory_SimpleMutex();
+  return category;
+}
+S<Type_SimpleMutex> CreateType_SimpleMutex(Params<0>::Type params) {
+  static const auto cached = S_get(new ExtType_SimpleMutex(CreateCategory_SimpleMutex(), Params<0>::Type()));
+  return cached;
+}
+S<TypeValue> CreateValue_SimpleMutex(S<Type_SimpleMutex> parent) {
+  return S_get(new ExtValue_SimpleMutex(parent, ParamTuple()));
+}
+
+#ifdef ZEOLITE_PUBLIC_NAMESPACE
+}  // namespace ZEOLITE_PUBLIC_NAMESPACE
+using namespace ZEOLITE_PUBLIC_NAMESPACE;
+#endif  // ZEOLITE_PUBLIC_NAMESPACE
