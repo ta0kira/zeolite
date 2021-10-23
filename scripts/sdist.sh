a8ef132d213b6043b7bd49d5bd985e3d8e6ea202
Kevin P. Barry <ta0kira@gmail.com>
2021-10-22 19:46:19 -0700

    Adds check to sdist.sh for missing files, and adds missing file to cabal package.

--- scripts/sdist.sh
+++ scripts/sdist.sh
@@ -4,15 +4,27 @@ set -u -e
 
 cd "$(dirname "$0")/.."
 
+required='/(README\.md|\.zeolite-module|.+\.(0r[ptx]|h|hpp|cc|cpp))$'
 sdist_file=$(cabal sdist | sed 1d)
 [[ $? -eq 0 ]] || exit $?
 
-nv_files=($(tar -tzf "$sdist_file" | cut -d/ -f2- | grep -v '/$' | fgrep -vxf <(git ls-files)))
+tracked_files=$(git ls-files)
+sdist_files=$(tar -tzf "$sdist_file" | cut -d/ -f2- | grep -v '/$')
 
+nv_files=($(echo "$sdist_files" | fgrep -vxf <(echo "$tracked_files") || true))
 if [[ -n "${nv_files-}" ]]; then
   echo "*** $(basename "$sdist_file") contains untracked files ***" 1>&2
   printf '%s\n' "${nv_files[@]}"
   rm -f "$sdist_file"
-else
-  echo "$sdist_file"
+  exit 1
 fi
+
+missing_files=($(echo "$tracked_files" | egrep "$required" | fgrep -vxf <(echo "$sdist_files") || true))
+if [[ -n "${missing_files-}" ]]; then
+  echo "*** $(basename "$sdist_file") is missing required files ***" 1>&2
+  printf '%s\n' "${missing_files[@]}"
+  rm -f "$sdist_file"
+  exit 1
+fi
+
+echo "$sdist_file"
